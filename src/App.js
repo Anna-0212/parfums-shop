6da29a9d72e05a65531187a0911dcba892dcc274
Анна Хагнес <Anna@MacBook-Air-Anna.local>
2022-10-07 17:29:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
+import image from "./shopping-cart.png";
 
 function App() {
+  const [parfums, setParfums] = useState(data);
+
+const [showText, setShowText] = useState(false);
+
+const removeParfum = (id) => {
+  console.log(id)
+  let newParfums = parfums.filter((parfum) => 
+  parfum.id !== id);
+  console.log(newParfums)
+  setParfums(newParfums)
+}
+
+const showTextClick = (item) => {
+  item.showMore = !item.showMore
+  setShowText(!showText)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Parfum's butik </h1>
+      </div>
+      <div className="container shop-cart">
+        <img className="shopping-cart"src={image} alt="shopping-cart" />
+      <h2> {parfums.length}  </h2>
+        {/* <h2> {parfums.length} items in your basket </h2> */}
+      </div>
+      {parfums.map(item => {
+        const {id, name, image, description, price} = item;
+      
+        return(
+        <div key = {id}>
+      <div className="container">
+        <h3>{id}. {name}</h3>
+      </div>
+      <div className="container">
+        <img src={image} alt="parfum" width="300px" />
+      </div>
+
+      <div className="container">
+        <p>{showText ? description : description.substring(0, 200) + "..."} <button onClick = {() => showTextClick(item)} className="btnShow"> {showText ? "Show less" : "Show more"}</button> </p>
+      </div>
+      
+      <div className="container">
+        <p className="price"> {price} </p>
+      </div>
+
+      <div className="container">
+        <button onClick = {() => removeParfum(id)}> REMOVE</button>
+      </div>
+
+      </div>
+        )
+})}
+
+<div className='container'>
+  <button onClick = {() => setParfums([])}  className="btn"> delete all</button>
+</div>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const data = [
+    {
+        id: 1,
+        name: "Chanel №5",
+        image: "https://images.unsplash.com/photo-1610113233329-1c73b6f7fe98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fHBhcmZ1bXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        description: "Chanel No 5 Parfum by Chanel is a Floral Aldehyde fragrance for women. Chanel No 5 Parfum was launched in 1921. The nose behind this fragrance is Ernest Beaux. Top notes are Aldehydes, Ylang-Ylang, Neroli, Bergamot and Lemon; middle notes are iris, Jasmine, Rose, Orris Root and Lily-of-the-Valley; base notes are Civet, Sandalwood, Amber, Musk, Moss, Vetiver, Vanilla and Patchouli.",
+        price: "$149.90",
+        showMore: false
+    },
+    {
+        id: 2,
+        name: "SUN di Gioia, Giorgio Armani",
+        image: "https://images.unsplash.com/photo-1547887537-6158d64c35b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHBhcmZ1bXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        description: "5 essences to reconnect with nature and feel joyful again. Through the initial perfume and two new fragrances, the Gioia Collection offers the joyful experience of beautiful, fascinating nature. The power of water captured in Acqua di Gioia is now joined the radiant happiness of the Sun di Gioia, and the exhilarating energy of, Air di Gioia.",
+        price: "$65.00",
+        showMore: false
+    },
+    {
+        id: 3,
+        name: "SÌ EAU DE PARFUM, Armani beauty",
+        image: "https://images.unsplash.com/photo-1533603208986-24fd819e718a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHBhcmZ1bXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        description: "At once chic, voluptuous, intense, and soft, Sì eau de parfum lingers on the skin and enchants the senses. The distinguished and elegant fragrance allows women to express their every emotion. Sì has a top note of blackcurrant nectar, a heart of freesia and Rose of Mai, with a musky blond wood base. This fragrancewas created for the modern woman who’s strong yet feminine, sophisticated yet charismatic. She embodies the very essence of chic Italian elegance and style.",
+        price: "$138.00",
+        showMore: false
+    },
+    {
+        id: 4,
+        name: "Parfums RP Rose Cerise Eau de",
+        image: "https://images.unsplash.com/photo-1648063626966-cc1ce9ef23ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHBhcmZ1bXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        description: "Rose Cerise by Parfums RP is a fragrant bouquet in which the rose plays the leading role. It is framed by jasmine and cherry blossom, honey-sweet notes of mimosa and powdery-woody accords of violet. White musk makes this bouquet especially elegant and refined. It brings freshness and purity to the Rose cerise fragrance.",
+        price: "$39.90",
+        showMore: false
+    },  
+    {
+        id: 5,
+        name: "CANDY Eau de Parfum, PRADA",
+        image: "https://images.unsplash.com/photo-1630573133526-8d090e0269af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBhcmZ1bXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        description: "Rethink reality with Prada CANDY Eau de Parfum, a joyful olfactory balance merging caramel accord with warm benzoin resin inside a cloud of white musk. A jolt to the system, this perfume triggers bugs of spontaneity in the software of life to open up exhilarating spaces of re-imagined reality.",
+        price: "$96.00",
+        showMore: false
+    },
+    {
+        id: 6,
+        name: "Versace EROS Eau De Parfum",
+        image: "https://images.unsplash.com/photo-1624798956425-ef88fc12b540?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBhcmZ1bXN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+        description: "Eros by Versace is a Aromatic Fougere fragrance for men. Eros was launched in 2012. The nose behind this fragrance is Aurelien Guichard. Top notes are Mint, Green Apple and Lemon; middle notes are Tonka Bean, Ambroxan and Geranium; base notes are Madagascar Vanilla, Virginian Cedar, Atlas Cedar, Vetiver and Oakmoss.",
+        price: "$76.50",
+        showMore: false
+    }
+  
+]
